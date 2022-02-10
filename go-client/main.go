51a848eff960bb8723f1c1130a2ecb0b8f764f42
devopstales <42894256+devopstales@users.noreply.github.com>
2022-02-10 18:42:45 +0100

    go-client: merge yamls

--- go-client/main.go
+++ go-client/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/url"
 	"os"
@@ -10,47 +11,35 @@ import (
 	"runtime"
 
 	"github.com/gin-gonic/gin"
+	pkgruntime "k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/tools/clientcmd"
+	"sigs.k8s.io/yaml"
+
+	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
+	clientcmdlatest "k8s.io/client-go/tools/clientcmd/api/latest"
+)
+
+const (
+	kubeConfigEnvName         = "KUBECONFIG2"
+	kubeConfigDefaultFilename = "~/.kube/config"
 )
 
 type request struct {
-	KubeUser struct {
-		Name string `json:"name"`
-		User struct {
-			AuthProvider struct {
-				Name   string `json:"name"`
-				Config struct {
-					ClientID     string `json:"client-id"`
-					IdpIssuerURL string `json:"idp-issuer-url"`
-					IDToken      string `json:"id-token"`
-					RefreshToken string `json:"refresh-token"`
-					ClientSecret string `json:"client-secret"`
-				} `json:"config"`
-			} `json:"auth-provider"`
-		} `json:"user"`
-	} `json:"kube_user"`
-	KubeCluster struct {
-		Name    string `json:"name"`
-		Cluster struct {
-			CertificateAuthorityData string `json:"certificate-authority-data"`
-			Server                   string `json:"server"`
-		} `json:"cluster"`
-	} `json:"kube_cluster"`
-	KubeContext struct {
-		Name    string `json:"name"`
-		Context struct {
-			Cluster string `json:"cluster"`
-			User    string `json:"user"`
-		} `json:"context"`
-	} `json:"kube_context"`
-	Context string `json:"context"`
+	Context                  string `json:"context"`
+	Server                   string `json:"server"`
+	CertificateAuthorityData string `json:"certificate-authority-data"`
+	ClientID                 string `json:"client-id"`
+	IDToken                  string `json:"id-token"`
+	RefreshToken             string `json:"refresh-token"`
+	IdpIssuerURL             string `json:"idp-issuer-url"`
+	ClientSecret             string `json:"client_secret"`
 }
 
 func main() {
 	// Get and validate the argument
-	var url string
 	if len(os.Args) == 2 {
 		if isValidUrl(os.Args[1]) {
-			url = os.Args[1]
+			OpenInBrowser(os.Args[1])
 		} else {
 			fmt.Println("Argument is not a valid url")
 			os.Exit(2)
@@ -60,25 +49,7 @@ func main() {
 		os.Exit(2)
 	}
 
-	// Open in browser
-	var err error
-	switch runtime.GOOS {
-	case "linux":
-		err = exec.Command("xdg-open", url).Start()
-	case "windows":
-		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
-	case "darwin":
-		err = exec.Command("open", url).Start()
-	default:
-		err = fmt.Errorf("unsupported platform")
-	}
-	if err != nil {
-		log.Fatal(err)
-		os.Exit(1)
-	}
-
 	// Start webserver for callback
-	//// Run background
 	gin.SetMode(gin.ReleaseMode)
 	router := gin.New()
 	router.POST("/", callback)
@@ -99,54 +70,192 @@ func isValidUrl(toTest string) bool {
 	return true
 }
 
-func callback(c *gin.Context) {
-	var dirname string
-	var filename string
-	homedir, err := os.UserHomeDir()
+func OpenInBrowser(url string) {
+	// Open in browser
+	var err error
+	switch runtime.GOOS {
+	case "linux":
+		err = exec.Command("xdg-open", url).Start()
+	case "windows":
+		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "darwin":
+		err = exec.Command("open", url).Start()
+	default:
+		err = fmt.Errorf("unsupported platform")
+	}
 	if err != nil {
 		log.Fatal(err)
 		os.Exit(1)
-	} else {
-		dirname = filepath.Join(homedir, ".kue")
-		filename = filepath.Join(dirname, "config")
-
-		fmt.Println(filename)
-		// test if exists
-		if _, err := os.Stat(dirname); os.IsNotExist(err) {
-			// dir does not exist so make dir
-			os.Mkdir(dirname, os.ModePerm)
-		} else if _, err := os.Stat(filename); os.IsNotExist(err) {
-			// file does not exist
-			// write to file function
-			fmt.Println(filename) // debug
-		} else {
-			// file exists so read file
-			file, err := os.Open(filename)
-			if err != nil {
-				log.Fatal(err)
-				os.Exit(1)
-			}
-			fmt.Println(file) // debug
-			// write to file function
-		}
 	}
-
-	/*if _, err := os.Stat(dirname); os.IsNotExist(err) {
-		// path/to/whatever does not exist
-	}*/
-
-	var request request
-	c.BindJSON(&request)
-	fmt.Println(string(request.KubeUser.Name))
-
-	c.JSON(200, "Client Get Data")
-	//os.Exit(0)
 }
 
-// https://dev.to/heroku/deploying-your-first-golang-webapp-11b3
-// https://go.dev/doc/tutorial/web-service-gin
-// https://www.sohamkamani.com/golang/how-to-build-a-web-application/
-// https://stackoverflow.com/questions/42247978/go-gin-gonic-get-text-from-post-request
-// https://chenyitian.gitbooks.io/gin-web-framework/content/docs/38.html
+func callback(c *gin.Context) {
+	var request request
+	// Debug
+	/*
+		body, _ := ioutil.ReadAll(c.Request.Body)
+		println(string(body))
+	*/
+	// Debug
+	c.BindJSON(&request)
+	// fmt.Println(string(request.Context)) // Debug
 
-//// https://mholt.github.io/json-to-go/
+	c.JSON(200, "Client Get Data")
+
+	// Read config
+	var fileExist bool
+	var kubeconfig string
+	fileExist, kubeconfig = GetKubeConfig()
+
+	if fileExist == true {
+		configOverrides, _ := ioutil.TempFile("", "")
+		defer os.Remove(configOverrides.Name())
+
+		// Get config from request and write to file
+		clientcmd.WriteToFile(createValidTestConfig(request), configOverrides.Name())
+
+		// merge files
+		loadingRules := clientcmd.ClientConfigLoadingRules{
+			Precedence: []string{kubeconfig, configOverrides.Name()},
+		}
+
+		mergedConfig, err := loadingRules.Load()
+		if err != nil {
+			fmt.Printf("Unexpected error: %v", err)
+		}
+		json, err := pkgruntime.Encode(clientcmdlatest.Codec, mergedConfig)
+		if err != nil {
+			fmt.Printf("Unexpected error: %v", err)
+		}
+		output, err := yaml.JSONToYAML(json)
+		if err != nil {
+			fmt.Printf("Unexpected error: %v", err)
+		}
+
+		fmt.Println(string(output)) // Debug
+		// Write to file
+	} else {
+		configOverrides, _ := ioutil.TempFile("", "")
+		defer os.Remove(configOverrides.Name())
+
+		// Get config from request and write to file
+		clientcmd.WriteToFile(createValidTestConfig(request), configOverrides.Name())
+
+		// merge file
+		loadingRules := clientcmd.ClientConfigLoadingRules{
+			Precedence: []string{configOverrides.Name()},
+		}
+
+		kubeConfig, err := loadingRules.Load()
+		if err != nil {
+			fmt.Printf("Unexpected error: %v", err)
+		}
+		json, err := pkgruntime.Encode(clientcmdlatest.Codec, kubeConfig)
+		if err != nil {
+			fmt.Printf("Unexpected error: %v", err)
+		}
+		output, err := yaml.JSONToYAML(json)
+		if err != nil {
+			fmt.Printf("Unexpected error: %v", err)
+		}
+
+		fmt.Println(string(output)) // Debug
+		// Write to file
+	}
+
+	os.Exit(0)
+}
+
+func GetKubeConfig() (bool, string) {
+	var fileExist bool
+	var kubeConfigFileName string
+
+	ConfigFilename := os.Getenv(kubeConfigEnvName)
+	if ConfigFilename == "" {
+		homedir, err := os.UserHomeDir()
+		if err != nil {
+			log.Fatal(err)
+			os.Exit(1)
+		} else {
+			var dirname string = filepath.Join(homedir, ".kube")
+			var filename string = filepath.Join(dirname, "config")
+
+			// test if filename exists
+			if _, err := os.Stat(dirname); os.IsNotExist(err) {
+				// dir does not exist so create it
+				fmt.Println("dir does not exist so create it") // Debug
+				os.Mkdir(dirname, 0755)
+				fileExist = false
+			} else if _, err := os.Stat(filename); os.IsNotExist(err) {
+				// file does not exist
+				fmt.Println("file does not exist") // Debug
+				fileExist = false
+			} else {
+				// file exists
+				// fmt.Println("file exist") // Debug
+				fileExist = true
+			}
+			kubeConfigFileName = filename
+		}
+	} else {
+		// test if kubeConfigEnvName exists
+		if _, err := os.Stat(kubeConfigFileName); os.IsNotExist(err) {
+			// file does not exist
+			fileExist = false
+		} else {
+			// file exists
+			fileExist = true
+		}
+		kubeConfigFileName = ConfigFilename
+	}
+	return fileExist, kubeConfigFileName
+}
+
+func createValidTestConfig(request request) clientcmdapi.Config {
+	/*
+		const (
+			// demo data for test
+			context          = "cl04"
+			k8s_server_url   = "https://anything.com:8080"
+			client_id        = "dashboard-proxy"
+			client_secret    = "df4c4725-98c9"
+			id_token         = "eyJhbGciOiJSUzI1NiIs..."
+			oauth_server_uri = "https://sso.anything.com/auth/realms/someting"
+			refresh_token    = "eyJhbGciOiJIUzI1NiIsInR5cCI___"
+			CA               = "-----BEGIN CERTIFICATE-----MIIELTCCApWgAwIBAgIRAODbJ0M+pR7t/0VsFRfGR5UwDQYJKoZIhvcNAQELBQAwLzEsfqPPVSmiR8W2eYJ8JQ=-----END CERTIFICATE-----"
+		)
+	*/
+
+	var (
+		kubeConfig = clientcmdapi.Config{
+			AuthInfos: map[string]*clientcmdapi.AuthInfo{
+				request.Context: {
+					AuthProvider: &clientcmdapi.AuthProviderConfig{
+						Name: "oidc",
+						Config: map[string]string{
+							"client-id":      request.ClientID,
+							"client-secret":  request.ClientSecret,
+							"id-token":       request.IDToken,
+							"idp-issuer-url": request.IdpIssuerURL,
+							"refresh-token":  request.RefreshToken,
+						},
+					},
+				},
+			},
+			Clusters: map[string]*clientcmdapi.Cluster{
+				request.Context: {
+					Server:                   request.Server,
+					CertificateAuthorityData: []byte(request.CertificateAuthorityData),
+				},
+			},
+			Contexts: map[string]*clientcmdapi.Context{
+				request.Context: {
+					AuthInfo: request.Context,
+					Cluster:  request.Context,
+				},
+			},
+		}
+	)
+
+	return kubeConfig
+}
