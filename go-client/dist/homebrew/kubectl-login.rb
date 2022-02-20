85b515f52ac0e0f6e797ac5a0a7e27ca523a23c4
devopstales <42894256+devopstales@users.noreply.github.com>
2022-02-20 11:11:44 +0100

    add kerw and berw release

--- go-client/dist/homebrew/kubectl-login.rb
+++ go-client/dist/homebrew/kubectl-login.rb
@@ -2,14 +2,14 @@ class KubectlLogin < Formula
   desc "An application that can be used to easily enable authentication flows via OIDC for a kubernetes cluster. "
   homepage "https://github.com/devopstales/kube-openid-connect/"
   license "Apache-2.0"
-  version "0.1"
+  version "#version#"
 
   if OS.mac?
-    url "https://github.com/devopstales/kube-openid-connect/releases/download/client_v0.1/kubectl-login_osx.tar.gz"
-    sha256 "bed8b7bf796129943adabcda66fd3574e508a384040074a1be3561a70f32cd81"
+    url "https://github.com/devopstales/kube-openid-connect/releases/download/client_v#version#/kubectl-login_osx.tar.gz"
+    sha256 "#checksum_osx#"
   elsif OS.linux?
-    url "https://github.com/devopstales/kube-openid-connect/releases/download/client_v0.1/kubectl-login_linux.tar.gz"
-    sha256 "f57602e427507b91b2b4a25fc5786c7514d3ea3243bd191f9feae6d79d69a9c7"
+    url "https://github.com/devopstales/kube-openid-connect/releases/download/client_v#version#/kubectl-login_linux.tar.gz"
+    sha256 "#checksum_linux#"
   end
 
   def install
